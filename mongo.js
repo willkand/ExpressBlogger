edec305dfc540648ba726c24ae8fd739da9d28f8
dd-code-immersives <domilamad89@gmail.com>
2023-02-08 14:22:58 -0500

    added mongo driver, deleted old version

---
+++ mongo.js
@@ -0,0 +1,25 @@
+const { MongoClient } = require("mongodb");
+
+let database;
+async function mongoConnect() {
+	// Connection URI
+	const uri = process.env.MONGO_URI;
+	// Create a new MongoClient
+	const client = new MongoClient(uri);
+	try {
+		// Connect the client to the server
+		await client.connect();
+		database = await client.db(process.env.MONGO_DATABASE);
+		// Establish and verify connection
+		console.log("db connected");
+	} catch (error) {
+		throw Error("Could not connect to MongoDB. " + error);
+	}
+}
+function db() {
+	return database;
+}
+module.exports = {
+	mongoConnect,
+	db,
+};
