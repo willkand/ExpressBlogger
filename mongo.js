6c071321891952bbea73e09b82b025a82ce0bd80
dd-code-immersives <domilamad89@gmail.com>
2023-02-08 14:24:06 -0500

    updated env variables

--- mongo.js
+++ mongo.js
@@ -3,13 +3,13 @@ const { MongoClient } = require("mongodb");
 let database;
 async function mongoConnect() {
 	// Connection URI
-	const uri = process.env.MONGO_URI;
+	const uri = process.env.ATLAS_URI;
 	// Create a new MongoClient
 	const client = new MongoClient(uri);
 	try {
 		// Connect the client to the server
 		await client.connect();
-		database = await client.db(process.env.MONGO_DATABASE);
+		database = await client.db("blog_data");
 		// Establish and verify connection
 		console.log("db connected");
 	} catch (error) {
